bd67d3f6e8c555b29db9fb7fe19b80b9ade6bc1a
alex argunov <sajkoooo@gmail.com>
2023-05-13 08:46:26 +0300

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -10,4 +10,10 @@
     programs.bat.config = {
   theme = "TwoDark";
 };
-    # fish}
+    # bash
+  programs.bash.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
+  programs.bash.initExtra = "source <(fleek completion bash)";
+  programs.bash.enableCompletion = true;
+  programs.bash.enableVteIntegration = true;
+  programs.bash.enable = true;
+}
