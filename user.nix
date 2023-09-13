f211ccaa7900cbdf4cbb08f5d8120467ab8a4061
Alexander Argunov <alexander.argunov@tern.com>
2023-09-13 18:11:21 +0300

    fleek: update flake.lock

--- user.nix
+++ user.nix
@@ -1,5 +1,6 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-  programs.exa.enableAliases = true;
+  # programs.exa.enableAliases = true;
+  programs.eza.enableAliases = true;
   programs.direnv.nix-direnv.enable = true;
 }
