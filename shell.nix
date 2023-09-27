5f6093d2ba3ebdbc77391a385a0255f114ab9922
Alexander Argunov <alexander.argunov@tern.com>
2023-09-27 14:21:24 +0300

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -10,4 +10,11 @@
     programs.bat.config = {
   theme = "TwoDark";
 };
-    # fish}
+    # zsh
+  programs.zsh.profileExtra = ''
+    [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
+    export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
+  '';
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enable = true;
+}
