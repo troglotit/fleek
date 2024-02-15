b9688b94cc49719d7e711d59624a525e8ceb2fbf
Alexander Argunov <alexander.argunov@tern.com>
2024-02-15 12:29:52 +0300

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -13,5 +13,5 @@
     # fish
   programs.fish.enable = true;
   # As per https://nixos.wiki/wiki/Fish setting fish as login shell may cause some issues
-  users.defaultUserShell = pkgs.fish;
+  # users.defaultUserShell = pkgs.fish;
 }
