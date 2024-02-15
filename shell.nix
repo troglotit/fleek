34aac682146e795fe95c63bc8235fc7f786e2492
Alexander Argunov <alexander.argunov@tern.com>
2024-02-15 11:38:59 +0300

    flake update

--- shell.nix
+++ shell.nix
@@ -10,11 +10,4 @@
     programs.bat.config = {
   theme = "TwoDark";
 };
-    # zsh
-  programs.zsh.profileExtra = ''
-    [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
-    export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
-  '';
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enable = true;
-}
+    # fish}
